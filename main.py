9b89f705c2f9f6dfbadad7431139f5a9dbb8e787
DRags <mrags14508@gmail.com>
2026-05-10 13:38:14 +0100

    feat: base functionality

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-import zipfile
+from zipfile import ZipFile
+import shutil
+from pathlib import Path
+from os import mkdir
+
+mkdir("packs/compressed-edit")
+
+with ZipFile("packs/base.zip", "r") as base_zip:
+    base_files: list[str] = base_zip.namelist()
+
+    with ZipFile("packs/edit.zip", "r") as edit_zip:
+        for file_name in base_files:
+            with base_zip.open(file_name) as base_content:
+                with edit_zip.open(file_name) as edit_content:
+                    if edit_content != base_content:
+                        with open(f"packs/compressed-edit/{file_name}", "wb") as f:
+                            f.write(edit_content.read())
+                    else:
+                        print(f"Detected copy file: `{file_name}`")
+
+folder = Path("packs/compressed-edit")
+files = []
+
+for path in folder.rglob("*"):
+    if path.is_file():
+        files.append(path)
+
+with ZipFile("packs/compressed-edit.zip", "w") as zip_ref:
+    for file_path in files:
+        zip_ref.write(file_path, arcname=file_path.relative_to(folder))
+
+shutil.rmtree(folder)
